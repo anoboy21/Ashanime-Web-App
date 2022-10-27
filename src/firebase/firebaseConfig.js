3fa6fed3f5032ac5e920046fbce5d0a9e08457a2
anoboy21 <109586478+anoboy21@users.noreply.github.com>
2022-10-27 18:24:45 +0800

    Create firebaseConfig.js

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBfRFncIkXLmKSWx8efBT-RcUiDmaCr6Gc",
+  authDomain: "topmovie-cd7f7.firebaseapp.com",
+  projectId: "topmovie-cd7f7",
+  storageBucket: "topmovie-cd7f7.appspot.com",
+  messagingSenderId: "552388803144",
+  appId: "1:552388803144:web:5c2adec1c48519035e767d",
+  measurementId: "G-FKRY4VMDFY"
+};
